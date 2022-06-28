6d16ac722932f80b965a344c670ae0d61f52b83f
JOMADELEMA <jomadelema@gmail.com>
2022-06-27 22:06:51 -0600

    se implementó el primer Layout de photography

--- src/Pages/Photography1.jsx
+++ src/Pages/Photography1.jsx
@@ -3,46 +3,46 @@ import { BrandInstagram, BrandFacebook } from "tabler-icons-react";
 
 const Photography1 = () => {
   return (
-    <div className=" bg-fondo-semuc bg-center bg-cover h-screen">
-      <div className="mx-20 grid grid-rows-6 lg:flex lg:flex-col h-screen lg:justify-between">
-        <div className="w-full row-start-6 self-center">
-          <div className="mt-3 border w-full flex lg:order-first justify-center lg:justify-end">
-            <p className="text-gray-50 hover:text-gray-500 text-2xl">
+    <div className=" bg-fondo-flower bg-center bg-cover h-screen">
+      <div className="h-screen flex flex-col justify-evenly lg:justify-between mx-10">
+        <div className="flex justify-center lg:justify-end  lg:order-1 order-2">
+          <div className="mt-3 w-full flex lg:order-first justify-center lg:justify-end">
+            <p className="text-gray-50 hover:underline text-lg lg:text-2xl cursor-pointer">
               johndoephotography.com
             </p>
           </div>
         </div>
 
-        <div className="w-full row-start-1 col-span-1">
-          <div className="border w-full flex flex-col order-first">
-            <div className="text-gray-50 text-5xl font-bold border self-center lg:self-end">
+        <div className="flex justify-center lg:justify-end  lg:order-2 h-full lg:h-auto">
+          <div className="w-full flex flex-col order-first">
+            <h1 className="text-gray-50 text-5xl lg:text-6xl font-bold self-center lg:self-end">
               Jhon Doe
-            </div>
-            <div className="text-gray-50 text-3xl font-semibold border self-center lg:self-end">
+            </h1>
+            <p className="text-gray-50 text-2xl lg:text-3xl font-semibold self-center lg:self-end">
               Photographer
-            </div>
+            </p>
 
-            <div className="flex mt-3 border justify-evenly lg:justify-end">
-              <p className="text-gray-50 font-semibold text-xl cursor-pointer hover:text-gray-600 lg:ml-10">
+            <div className="flex mt-3 justify-evenly lg:justify-end">
+              <p className="text-gray-50 font-semibold text-lg lg:text-xl cursor-pointer hover:text-gray-300 hover:underline lg:ml-10">
                 Portfolio
               </p>
-              <p className="text-gray-50 font-semibold text-xl cursor-pointer hover:text-gray-600 lg:ml-10">
+              <p className="text-gray-50 font-semibold text-lg lg:text-xl cursor-pointer hover:text-gray-300 hover:underline lg:ml-10">
                 Info
               </p>
-              <p className="text-gray-50 font-semibold text-xl cursor-pointer hover:text-gray-600 lg:ml-10">
+              <p className="text-gray-50 font-semibold text-lg lg:text-xl cursor-pointer hover:text-gray-300 hover:underline lg:ml-10">
                 About
               </p>
-              <p className="text-gray-50 font-semibold text-xl cursor-pointer hover:text-gray-600 lg:ml-10">
+              <p className="text-gray-50 font-semibold text-lg lg:text-xl cursor-pointer hover:text-gray-300 hover:underline lg:ml-10">
                 Contact
               </p>
             </div>
           </div>
         </div>
 
-        <div className="w-full row-start-6 self-end">
-          <div className="flex border w-full order-last mb-3 justify-center lg:justify-end">
-            <BrandInstagram color="white" className="cursor-pointer" />
-            <BrandFacebook color="white" className="cursor-pointer" />
+        <div className="flex justify-center lg:justify-end  order-3">
+          <div className="flex w-full order-last mb-3 justify-center lg:justify-end">
+            <BrandInstagram color="white" className="cursor-pointer" size={35} strokeWidth={1} />
+            <BrandFacebook color="white" className="cursor-pointer" size={35} strokeWidth={1} />
           </div>
         </div>
       </div>
---
+++ src/Pages/Photography2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Photography2 = () => {
+  return (
+    <div>Photography2</div>
+  )
+}
+
+export default Photography2
