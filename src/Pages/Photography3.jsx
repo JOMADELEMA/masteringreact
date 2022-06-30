a885d75877a2ad83380beb967ecc9759ace8adc6
JOMADELEMA <jomadelema@gmail.com>
2022-06-29 22:56:57 -0600

    se hizo correcciones y preparativos

--- src/Pages/Photography3.jsx
+++ src/Pages/Photography3.jsx
@@ -21,12 +21,10 @@ const Photography3 = () => {
             <div id="contenedor" className='flex'>
 
                 {/* Sidebar */}
-                <div id="sidebar" className='h-screen w-72 flex flex-col shadow-md'>
-
+                <div id="sidebar-lg" className='h-screen w-72 flex flex-col shadow-md'>
                     <div id='logo' className='flex justify-center'>
                         <Camera size={150} color='red' strokeWidth={0.5} className="hover:grayscale cursor-pointer" />
                     </div>
-
                     <div id='enlaces-principales' className=' mt-10 mx-2'>
                         <ul>
                             <li className='font-semibold hover:bg-gray-200 hover:text-black cursor-pointer'>FOTOGRAFIAS</li>
@@ -41,37 +39,56 @@ const Photography3 = () => {
                             <li className='font-light hover:bg-gray-200 hover:text-black cursor-pointer'>CONTACTO</li>
                         </ul>
                     </div>
+                </div>
 
+                <div id="sidebar-md" className='h-screen w-72 flex flex-col shadow-md'>
+                    <div id='logo' className='flex justify-center'>
+                        <Camera size={150} color='red' strokeWidth={0.5} className="hover:grayscale cursor-pointer" />
+                    </div>
+                    <div id='enlaces-principales' className=' mt-10 mx-2'>
+                        <ul>
+                            <li className='font-semibold hover:bg-gray-200 hover:text-black cursor-pointer'>FOTOGRAFIAS</li>
+                            <li className='font-semibold hover:bg-gray-200 hover:text-black cursor-pointer'>PELICULAS</li>
+                        </ul>
+                    </div>
+                    <div id='enlaces-secundarios' className=' mt-10 mx-2'>
+                        <ul>
+                            <li className='font-light hover:bg-gray-200 hover:text-black cursor-pointer'>TIENDA</li>
+                            <li className='font-light hover:bg-gray-200 hover:text-black cursor-pointer'>MUSEO</li>
+                            <li className='font-light hover:bg-gray-200 hover:text-black cursor-pointer'>BLOG</li>
+                            <li className='font-light hover:bg-gray-200 hover:text-black cursor-pointer'>CONTACTO</li>
+                        </ul>
+                    </div>
                 </div>
 
                 {/* Grid */}
                 <div id="contenedor-grid" className='w-full h-screen border overflow-auto'>
                     <div id="row" className='flex flex-wrap'>
                         <div id="column " className='lg:flex-25 lg:max-w-25 md:flex-50 md:max-w-50 px-1'>
-                            <img src={pic9}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic1}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic6}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic10}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic9} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic1} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic6} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic10} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
                         </div>
                         <div id="column " className='lg:flex-25 lg:max-w-25 md:flex-50 md:max-w-50 px-1'>
-                            <img src={pic1}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic2}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic8}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic4}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic1} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic2} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic8} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic4} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
 
                         </div>
                         <div id="column " className='lg:flex-25 lg:max-w-25 md:flex-50 md:max-w-50 px-1'>
-                            <img src={pic5}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic3} className="mt-2 align-middle w-full  rounded-md"  alt="pic1" />
-                            <img src={pic7} className="mt-2 align-middle w-full  rounded-md"  alt="pic1" />
-                            <img src={pic8} className="mt-2 align-middle w-full  rounded-md"  alt="pic1" />
+                            <img src={pic5} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic3} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic7} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic8} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
 
                         </div>
                         <div id="column " className='lg:flex-25 lg:max-w-25 md:flex-50 md:max-w-50 px-1'>
-                            <img src={pic3}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic2}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic8}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
-                            <img src={pic4}  className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic3} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic2} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic8} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
+                            <img src={pic4} className="mt-2 align-middle w-full  rounded-md" alt="pic1" />
 
                         </div>
 
